6020275ac7db5c6e27d4c87cfc7077a0ff6b0660
yanchuan <ysim@cs.cmu.edu>
2016-12-17 19:23:36 +0800

    refactor code

--- functions/pdf_to_text/pdf_to_text.py
+++ functions/pdf_to_text/pdf_to_text.py
@@ -15,21 +15,20 @@ LIB_DIR = os.path.join(LAMBDA_TASK_ROOT, 'lib-linux_x64')
 s3_client = boto3.client('s3')
 
 
-def pdf_to_text(event, context):
-    # download file from s3_pdf
+def doc_to_text(event, context):
     bucket = event['bucket']
     doc_key = event['key']
 
     doc_id, doc_ext = os.path.splitext(doc_key)
 
-    with NamedTemporaryFile(prefix='intelllex_', suffix='.pdf', delete=False) as f:
+    with NamedTemporaryFile(prefix='intelllex_', suffix='.doc', delete=False) as f:
         doc_path = f.name
     #end with
 
     try: s3_client.download_file(bucket, doc_key, doc_path)
     except Exception as e: return dict(success=False, reason=u'Exception while getting document from S3: {}'.format(e))
 
-    cmdline = [os.path.join(BIN_DIR, 'pdftotext'), '-nopgbrk', doc_path]
+    cmdline = [os.path.join(BIN_DIR, 'doctotext'), '-nopgbrk', doc_path]
     try: subprocess.check_call(cmdline, shell=False)
     except subprocess.CalledProcessError as e: return dict(success=False, reason=u'Exception while executing {}: {}'.format(cmdline, e))
 
@@ -46,7 +45,7 @@ def main():
     parser = ArgumentParser(description='Extract text from binary documents.')
     parser.parse_args()
 
-    print pdf_to_text(dict(bucket='docbot-test-lambda', key='text.pdf'), {})
+    print doc_to_text(dict(bucket='docbot-test-lambda', key='text.doc'), {})
 #end def
 
 
